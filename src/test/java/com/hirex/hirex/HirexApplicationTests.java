a86e1a57ba933736ad129b6dde9b74f784a38dbf
Alok Ranjan Joshi <alokranjanjoshi07567@gmail.com>
2026-02-11 20:24:03 +0530

    created the initial structure of the projects

---
+++ src/test/java/com/hirex/hirex/HirexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirex.hirex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
